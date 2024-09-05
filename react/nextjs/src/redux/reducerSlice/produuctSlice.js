8d7e37f13ad0bff7065f4e4118c80fc81c7fa95d
ankitaakc <156278868+ankitaakc@users.noreply.github.com>
2024-09-05 12:59:15 +0545

    added login page

---
+++ react/nextjs/src/redux/reducerSlice/produuctSlice.js
@@ -0,0 +1,62 @@
+import { createSlice } from '@reduxjs/toolkit'
+export const productSlice = createSlice({
+  name: 'box',
+  initialState: {
+    width: 40,
+    height: 40,
+    backgroundColor: 'red',
+    borderRadius: 0,
+    padding:20,
+    margin:20,
+    marginLeft: 50,
+    marginTop: 50,
+    isCircle: false
+  },
+  reducers: {
+    changeShape: (state, action) => {
+      if(state.borderRadius == '50%'){
+        state.borderRadius=0
+      }else{
+        state.height=state.width
+        state.borderRadius= '50%'
+      }
+   
+    },
+    increaseWidth: (state, action) => {
+      return {
+        ...state,
+        width: state.width+20,
+        height:  state.borderRadius=== '50%' ? state.width+20: state.height
+      }
+
+   },
+   decreaseWidth: (state, action) => {
+    return {
+      ...state,
+      width: state.width-20,
+      height:  state.borderRadius=== '50%' ? state.width-20: state.height
+    }
+
+ },
+   changeBackgroundColor: (state, action) => {
+    state.backgroundColor = action.payload
+
+ },
+ moveRight: (state, action) => {
+  state.marginLeft= state.marginLeft+20
+},
+moveLeft: (state, action) => {
+  state.marginLeft= state.marginLeft-20
+},
+moveBottom: (state, action) => {
+  state.marginTop =  state.marginTop +20
+},
+moveUp: (state, action) => {
+  state.marginTop =  state.marginTop -20
+},
+  }
+})
+
+export const { changeShape,moveUp,decreaseWidth,increaseWidth,moveBottom,changeBackgroundColor, moveRight,moveLeft } = boxSlice.actions
+
+export default productSlice.reducer
---
+++ react/nextjs/src/redux/store.js
@@ -0,0 +1,13 @@
+import { configureStore } from '@reduxjs/toolkit'
+import counterSlice from './reducerSlice/counterSlice'
+import boxSlice from './reducerSlice/boxSlice'
+
+
+
+export default configureStore({
+  reducer: {
+    counter: counterSlice,
+    box: boxSlice,
+    user: userSlice
+  }
+})
